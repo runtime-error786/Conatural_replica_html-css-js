9b331c5118c2c34e7834092319cbb5e3e7c04259
Mustafa Rizwan <mustafa782a@gmail.com>
2023-07-14 15:04:27 +0500

    i made first page navbar

---
+++ front_page.js
@@ -0,0 +1,43 @@
+let f = document.getElementById("fc");
+let yo = document.getElementById("yo");
+let i = document.getElementById("in");
+f.addEventListener("mouseover", function(){
+    let f2 = document.getElementById("n1");
+    console.log("over");
+    f2.style.display = "block";
+});
+
+f.addEventListener("mouseout", function(){
+    let f2 = document.getElementById("n1");
+    console.log("out");
+    f2.style.display = "none";
+});
+
+yo.addEventListener("mouseover", function(){
+    let f2 = document.getElementById("n3");
+    console.log("over");
+    f2.style.display = "block";
+});
+
+yo.addEventListener("mouseout", function(){
+    let f2 = document.getElementById("n3");
+    console.log("out");
+    f2.style.display = "none";
+});
+
+i.addEventListener("mouseover", function(){
+    let f2 = document.getElementById("n2");
+    console.log("over");
+    f2.style.display = "block";
+});
+
+i.addEventListener("mouseout", function(){
+    let f2 = document.getElementById("n2");
+    console.log("out");
+    f2.style.display = "none";
+});
+
+let g = document.getElementById("w1");
+g.addEventListener("click" , function(){
+    location.reload();
+});
